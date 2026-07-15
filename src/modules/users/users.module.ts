c37faba8cfaa394ef9751922ca7ee6343f86b04e
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2026-07-15 14:19:37 +0300

    update.

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './domain/entities/user.entity';
 import { UsersController } from './presentation/controller/users.controller';
-
+import { UsersService } from './application/services/users.service';
+import { UsersRepositoryService } from './infrastructure/repositories/users.repository.service';
+import { UsersRepository } from './domain/repository/users.repository';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  exports: [TypeOrmModule],
-  providers: [],
+  exports: [UsersRepository],
+  providers: [
+    UsersService,
+    {
+      provide: UsersRepository,
+      useClass: UsersRepositoryService,
+    },
+  ],
 })
 export class UsersModule {}
